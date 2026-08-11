05bfae2c027f5b7785f3f0e7a82962bb8a5058c5
Jim Park <auclown@gmail.com>
2026-08-11 21:02:11 +1200

    GCP GCS bucket added for tf state management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
+
+  # Configure Centralized Remote State in GCS
+  backend "gcs" {
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -30,5 +36,10 @@ resource "google_project_service" "secret_manager" {
 
 resource "google_project_service" "iam_credentials" {
   service            = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam_api" {
+  service            = "iam.googleapis.com"
   disable_on_destroy = false
 }
