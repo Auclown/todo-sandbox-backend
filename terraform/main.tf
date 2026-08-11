b2a83670842d3b78cafc71dcd5a1e1ecd6e2110c
Jim Park <auclown@gmail.com>
2026-08-11 21:05:34 +1200

    Cloud resource manager API enabled

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,3 +43,8 @@ resource "google_project_service" "iam_api" {
   service            = "iam.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "cloud_resource_manager" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
