633b0889b1ef83d4bcde39d3cfad76e59ca9df94
Jim Park <auclown@gmail.com>
2026-08-12 00:05:04 +1200

    Allowing GitHub Actions dynamic runners

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -8,6 +8,13 @@ resource "google_container_cluster" "primary" {
   # Prevents Terraform from hanging if backend gets deleted later
   deletion_protection = false
 
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block = "0.0.0.0/0"
+      display_name = "Allow GitHub Actions dynamic runners"
+    }
+  }
+
   depends_on = [
     google_project_service.container_api
   ]
