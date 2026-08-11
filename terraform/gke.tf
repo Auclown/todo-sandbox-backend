88ddfa0f07d26b41dff0eea156bbf9902c6df74d
Jim Park <auclown@gmail.com>
2026-08-12 00:07:39 +1200

    Ignoring Trivy for now

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,3 +1,4 @@
+#trivy:ignore:GCP-0061
 resource "google_container_cluster" "primary" {
   name     = "todo-cluster"
   location = var.region
@@ -7,13 +8,6 @@ resource "google_container_cluster" "primary" {
 
   # Prevents Terraform from hanging if backend gets deleted later
   deletion_protection = false
-
-  master_authorized_networks_config {
-    cidr_blocks {
-      cidr_block   = "0.0.0.0/0"
-      display_name = "Allow GitHub Actions dynamic runners"
-    }
-  }
 
   depends_on = [
     google_project_service.container_api
