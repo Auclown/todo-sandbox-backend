d483e1c440fb72b6b23838756b730b495685c639
Jim Park <auclown@gmail.com>
2026-08-24 15:09:18 +1200

    Typo fixed. Reviving GCP resources for frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  # Configure Centralized Remote State in GCS
+  # Configure Centralised Remote State in GCS
   backend "gcs" {
     prefix = "terraform/state"
   }
